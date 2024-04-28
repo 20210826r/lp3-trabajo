551b3616000de982700177908d973ac0abbae8ec
renato <20210826@lamolina.edu.pe>
2024-04-28 18:17:42 -0500

    avance del sistema

--- nutricion.pl
+++ nutricion.pl
@@ -1,8 +1,46 @@
-
 %modulo principal
 
 :-ensure_loaded(diagnostico).
 :-ensure_loaded(imc).
+
+:-dynamic(nutricion/2).
+:-dynamic(sintoma/2).
+
+main:-
+    inicio,
+    problemas([]).
+
+
+inicio:-
+    nl,
+    write('\n---------------------------------------------\n'),
+    write('Prolog Sistema experto en Calidad Alimentativa'),
+    write('\n---------------------------------------------\n'),
+    nl.
+
+problemas(Problemas):-
+    write('Escriba sus síntomas'),nl,
+    write('Posibles problemas: desgano - fatiga - irritabilidad - debilidad - etc'),
+    nl,
+    write('Al finlizar los sintomas escriba "Stop"'),nl,
+    retractall(nutricion(_,Problemas)),
+    leer_problemas(Lista),
+    forall(member(X,Lista),assertz(nutricion(_,X))),
+    write(Lista),
+    nutricion(_,Problemas).
+
+leer_problemas(Problemas):-
+    write('Ingrese su problema: \n'),
+    read_string(user,"\n","\r",_,Respuesta),
+    (
+    Respuesta == "Stop"
+        ->
+        Problemas = []
+        ;
+        leer_problemas(Problemas1),
+        Problemas = [Respuesta|Problemas1]
+    ).
+
 
 
 
